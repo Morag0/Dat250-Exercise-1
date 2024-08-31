186cd37316e7f4b1a408ef809ba592d73c513b08
Morten Ågotnes <morten.aagotnes@gmail.com>
2024-08-31 11:37:31 +0200

    Add files via upload

---
+++ demo/src/test/java/Morten/Aagotnes/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Morten.Aagotnes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
